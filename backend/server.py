02317ec616f1257642453f4e3ee30ec880434724
Kevin <kevinros1190@gmail.com>
2023-07-12 11:58:31 -0400

    removed neural folder and redundant elastic call in server

--- backend/server.py
+++ backend/server.py
@@ -40,14 +40,6 @@ with app.app_context():
 	get_db()
 	get_redis()
 
-# Connect to elastic
-elastic_manager = ElasticManager(os.environ["elastic_username"],
-								 os.environ["elastic_password"],
-								 os.environ["elastic_domain"],
-								 os.environ["elastic_index_name"],
-								 None,
-								 "submissions")
-
 # for nltk data, used for parsing queries
 nltk.download("brown")
 nltk.download("punkt")
