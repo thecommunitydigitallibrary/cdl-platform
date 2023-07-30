cc62491fd3c072b281e0fc63dd3ee8dc191055b1
Rakshana <rj17@illinois.edu>
2023-07-30 15:13:51 -0500

    header changes + logo

--- frontend/website/components/homepage/hero.js
+++ frontend/website/components/homepage/hero.js
@@ -30,26 +30,48 @@ const Hero = () => {
             The CDL is a platform for bookmarking webpages, taking notes, and discovering online content. Our vision is to anticipate the questions that you may have while viewing online content and proactively provide you with answers without requiring any effort.
             </p>
 
-            <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
+            <div className="flex flex-col mb-3 items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
              
-              {!loggedOut ? <a href="/" className="px-8 py-4 text-lg font-medium text-center text-white bg-blue-500 rounded-md no-underline">
+              {!loggedOut ? <a href="/" className="px-8 py-3 text-lg font-medium text-center text-white bg-blue-500 rounded-md no-underline">
                       Home
-                    </a> : <a href="/auth" className="px-8 py-4 text-lg font-medium text-center text-white bg-blue-500 rounded-md no-underline">
+                    </a> : <a href="/auth" className="px-8 py-3 text-lg font-medium text-center text-white bg-blue-500 rounded-md no-underline">
                       Get Started
                     </a>}
+
               <a
                 href="https://chrome.google.com/webstore/detail/the-community-digital-lib/didjjbenidcdopncjajdoeniaplicdee?hl=en&authuser=0"
                 target="_blank"
                 rel="noopener"
-                className="flex items-center space-x-2 text-gray-500 dark:text-gray-400 no-underline">
+                className="flex items-center space-x-2 text-gray-500 dark:text-gray-400 px-8 py-3 text-white bg-green-500 text-lg font-medium text-center rounded-md  no-underline">
                   <svg role="img"
                   width="24"
                   height="24"
                   className="w-5 h-5"
                   viewBox="0 0 24 24"
-                  fill="currentColor" ><title>Google Chrome</title><path d="M12 0C8.21 0 4.831 1.757 2.632 4.501l3.953 6.848A5.454 5.454 0 0 1 12 6.545h10.691A12 12 0 0 0 12 0zM1.931 5.47A11.943 11.943 0 0 0 0 12c0 6.012 4.42 10.991 10.189 11.864l3.953-6.847a5.45 5.45 0 0 1-6.865-2.29zm13.342 2.166a5.446 5.446 0 0 1 1.45 7.09l.002.001h-.002l-5.344 9.257c.206.01.413.016.621.016 6.627 0 12-5.373 12-12 0-1.54-.29-3.011-.818-4.364zM12 16.364a4.364 4.364 0 1 1 0-8.728 4.364 4.364 0 0 1 0 8.728Z" fill="#595454"></path></svg>
+                  fill="white" >
+                    <title>Google Chrome</title>
+                    <path d="M12 0C8.21 0 4.831 1.757 2.632 4.501l3.953 6.848A5.454 5.454 0 0 1 12 6.545h10.691A12 12 0 0 0 12 0zM1.931 5.47A11.943 11.943 0 0 0 0 12c0 6.012 4.42 10.991 10.189 11.864l3.953-6.847a5.45 5.45 0 0 1-6.865-2.29zm13.342 2.166a5.446 5.446 0 0 1 1.45 7.09l.002.001h-.002l-5.344 9.257c.206.01.413.016.621.016 6.627 0 12-5.373 12-12 0-1.54-.29-3.011-.818-4.364zM12 16.364a4.364 4.364 0 1 1 0-8.728 4.364 4.364 0 0 1 0 8.728Z" fill="#595454">
+                      </path></svg>
 
                 <span> Download Extension</span>
+              </a>
+              <a
+                href="https://github.com/thecommunitydigitallibrary/cdl-platform"
+                target="_blank"
+                rel="noopener"
+                className="flex items-center space-x-2 text-gray-500 dark:text-gray-400">
+                <svg
+                  role="img"
+                  width="24"
+                  height="24"
+                  className="w-5 h-5"
+                  viewBox="0 0 24 24"
+                  fill="currentColor"
+                  xmlns="http://www.w3.org/2000/svg">
+                  <title>GitHub</title>
+                  <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+                </svg>
+                <span> View on Github</span>
               </a>
             </div>
           </div>
@@ -58,7 +80,7 @@ const Hero = () => {
           <div className="">
             <Image
               src={heroImg}
-              width="500"
+              width="600"
               height="500"
               className={"object-cover"}
               alt="Hero"
