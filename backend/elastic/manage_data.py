30f008119ed9636ab15f8b26251bef8677a52168
Kevin <kevinros1190@gmail.com>
2023-09-11 13:37:35 -0500

    revert encoding to mitigate latin subset

--- backend/elastic/manage_data.py
+++ backend/elastic/manage_data.py
@@ -379,10 +379,11 @@ class ElasticManager:
     def postprocess(self, text):
 
         try:
-            text = text.encode("latin1", errors="ignore").decode("utf8", errors="ignore")
+            text = text.encode("latin1", errors="strict").decode("utf8", errors="strict")
         except Exception as e:
             print(e)
             traceback.print_exc()
+            text = text.encode("utf8", errors="ignore").decode("utf8", errors="ignore")
         
         try:
             hits = json.loads(text)["hits"]
