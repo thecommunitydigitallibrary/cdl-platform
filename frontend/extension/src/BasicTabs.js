90dc0036e6a998836f5b47cd94bbdb39735c041b
theashwin <me@ashwin.dev>
2023-09-17 15:46:49 -0500

    Extension Changes: Added Settings Tab on Home and Login Pages, Visual Changes and other small formatting changes

--- frontend/extension/src/BasicTabs.js
+++ frontend/extension/src/BasicTabs.js
@@ -10,13 +10,12 @@ import SettingsTab from './SettingsTab';
 import UploadFileOutlinedIcon from '@mui/icons-material/UploadFileOutlined';
 import SettingsIcon from '@mui/icons-material/Settings';
 import LogoutIcon from '@mui/icons-material/Logout';
-import Button from '@mui/material/Button';
-import Login from './Login'
-import { useNavigate } from 'react-router-dom'
+import {useNavigate} from 'react-router-dom'
 import FindInPageIcon from '@mui/icons-material/FindInPage';
+import HomeTabs from "./HomeTabs";
 
 function TabPanel(props) {
-  const { children, value, index, ...other } = props;
+  const {children, value, index, ...other} = props;
 
   return (
     <div
@@ -27,7 +26,7 @@ function TabPanel(props) {
       {...other}
     >
       {value === index && (
-        <Box sx={{ p: 2 }}>
+        <Box sx={{p: 2}}>
           <Typography>{children}</Typography>
         </Box>
       )}
@@ -49,20 +48,26 @@ function a11yProps(index) {
 }
 
 export default function BasicTabs() {
-  const [value, setValue] = React.useState(localStorage.getItem('defaultTab') === "submit" ? 1 : 0);
-  const [url, setUrl]=React.useState();
-  const [highlightedText, setHighlightedText]=React.useState();
+  const [value, setValue] = React.useState(localStorage.getItem('defaultTab') === "submit" ? 2 : 1);
+  const [url, setUrl] = React.useState();
+  const [highlightedText, setHighlightedText] = React.useState();
 
 
-  let setUrlState=(url, highlight)=>{
-     console.log(url, highlight)
-     setUrl(url);
-     setHighlightedText(highlight);
+  let setUrlState = (url, highlight) => {
+    console.log(url, highlight)
+    setUrl(url);
+    setHighlightedText(highlight);
   }
 
   let navigate = useNavigate()
 
   const handleChange = (event, newValue) => {
+    if (newValue === 0) {
+      setValue(1);
+      window.open(localStorage.getItem('backendSource'), "_blank", "noopener noreferrer");
+    } else if (newValue === 4) {
+      logout();
+    }
     setValue(newValue);
   };
 
@@ -72,34 +77,40 @@ export default function BasicTabs() {
   }
 
   return (
-    <div>
-      {localStorage.getItem('authToken') === null ? <Login /> : (
-      
-      <Box sx={{ width: '100%' }}>
-        <Box style={{ float: "center", marginTop: "12px" }}>
-            <a href={localStorage.getItem('backendSource')} style={{ fontSize: 20 }} target="_blank" rel="noopener noreferrer">
-              The Community Digital Library
-            </a>
-            <Button onClick={logout}><LogoutIcon/></Button>
-        </Box>
+    <div style={{margin: 0}}>
+      {localStorage.getItem('authToken') === null ? <HomeTabs/> : (
 
-        <Box sx={{ borderBottom: 1, borderColor: 'divider', float: "center" }}>
-          <Tabs value={value} onChange={handleChange} aria-label="basic tabs example" style={{ textAlign: "center" }}>
-            <Tab style = {{width: "40%", float:"left"}} label={<div><FindInPageIcon style={{ verticalAlign: 'middle' }} /> Search </div>} {...a11yProps(2)} />
-            <Tab style = {{width: "40%", float:"center"}} label={<div><UploadFileOutlinedIcon style={{ verticalAlign: 'middle' }} /> Submit </div>} {...a11yProps(1)} />
-            <Tab style = {{width: "20%", float:"left"}} label={<div><SettingsIcon style={{ verticalAlign: 'middle' }} /></div>} {...a11yProps(3)} />
-          </Tabs>
-        </Box>
-        <TabPanel value={value} index={0}>
-          <SearchTab setUrlState={setUrlState}/>
-        </TabPanel>
-        <TabPanel value={value} index={1}>
-          <SubmitTab setUrlState={setUrlState}/>
-        </TabPanel>
-        <TabPanel value={value} index={2}>
-          <SettingsTab setUrlState={setUrlState}/>
-        </TabPanel>
-      </Box>)}
+        <Box sx={{width: '100%'}}>
+
+          <Box sx={{borderBottom: 1, borderColor: 'divider', float: "center"}}>
+            <Tabs value={value} onChange={handleChange} aria-label="basic tabs example" style={{textAlign: "center"}}>
+              <Tab style={{width: "25%", float: "left"}}
+                   label={<div style={{padding: 0, margin: 0}}><h3 style={{margin: 0}}>CDL</h3>
+                   </div>} {...a11yProps(0)} />
+              <Tab style={{width: "25%", float: "left"}}
+                   label={<div><FindInPageIcon style={{verticalAlign: 'middle'}}/> Search </div>} {...a11yProps(1)} />
+              <Tab style={{width: "25%", float: "left"}}
+                   label={<div><UploadFileOutlinedIcon style={{verticalAlign: 'middle'}}/> Submit
+                   </div>} {...a11yProps(2)} />
+              <Tab style={{width: "12.5%", float: "left", minWidth: "0px"}}
+                   label={<div><SettingsIcon style={{verticalAlign: 'middle'}}/></div>} {...a11yProps(3)} />
+              <Tab style={{width: "12.5%", float: "left", minWidth: "0px"}}
+                   label={<div><LogoutIcon style={{verticalAlign: 'middle'}}/></div>} {...a11yProps(4)} />
+            </Tabs>
+          </Box>
+          <TabPanel value={value} index={0}>
+
+          </TabPanel>
+          <TabPanel value={value} index={1}>
+            <SearchTab setUrlState={setUrlState}/>
+          </TabPanel>
+          <TabPanel value={value} index={2}>
+            <SubmitTab setUrlState={setUrlState}/>
+          </TabPanel>
+          <TabPanel value={value} index={3}>
+            <SettingsTab setUrlState={setUrlState}/>
+          </TabPanel>
+        </Box>)}
     </div>
   );
 }
