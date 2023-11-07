c5b9f87ecafeee10f8f5ca855918e8b36669e910
Kevin <kevinros1190@gmail.com>
2023-11-07 11:09:33 -0600

    beginning of forum update

--- backend/app/views/communities.py
+++ backend/app/views/communities.py
@@ -7,11 +7,12 @@ import traceback
 from app.db import *
 from app.helpers.status import Status
 from app.helpers import response
-from app.helpers.helpers import token_required, format_time_for_display
+from app.helpers.helpers import token_required, format_time_for_display, sanitize_input
 from app.models.communities import Communities, Community
 from app.models.community_logs import CommunityLogs
 from app.models.users import Users
 from app.models.judgment import *
+from app.models.logs import Logs
 from app.views.logs import log_community_action
 
 communities = Blueprint('communities', __name__)
