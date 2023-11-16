613605039bb5dfc6a74488571affad51e8a2286a
Kevin <kevinros1190@gmail.com>
2023-11-16 16:37:09 -0600

    updated release log

--- frontend/website/pages/releaselog.js
+++ frontend/website/pages/releaselog.js
@@ -23,6 +23,23 @@ export default function ReleaseLog() {
       </h1>
 
       <h2 className="text-3xl mb-4"  style={{ margin: "20px 0px 0px 0px" }}>
+        November 17th, 2023
+      </h2>
+      <h5 className="text-2xl font-bold mb-3" style={{ margin: "10px 0px 0px 0px" }}>
+        Features
+      </h5>
+        <ul className="list-disc pl-6">
+            <li className="list-item">Auto-complete on website search bar header</li>
+        </ul>
+      <h5 className="text-2xl font-bold mb-3" style={{ margin: "10px 0px 0px 0px" }}>
+        Bug Fixes
+      </h5>
+        <ul className="list-disc pl-6">
+            <li className="list-item">Clicking whitespace around search result no longer redirects</li>
+            <li className="list-item">Clicking relevance judgment on search result no longer redirects</li>
+        </ul>
+
+      <h2 className="text-3xl mb-4"  style={{ margin: "20px 0px 0px 0px" }}>
         November 14th, 2023
       </h2>
       <h5 className="text-2xl font-bold mb-3" style={{ margin: "10px 0px 0px 0px" }}>
