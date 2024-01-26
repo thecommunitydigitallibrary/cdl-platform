811fe65f28ed482849ecd2f8c07f18bb6b148b0f
Kedar <kedar.takwane@gmail.com>
2024-01-26 21:15:59 +0530

    Removed test statements.

--- frontend/website/components/header.js
+++ frontend/website/components/header.js
@@ -132,12 +132,8 @@ function Header(props) {
     // Function for validating each submission. Checks if each key is present,
     // and also checks if the values are okay or not.
     async function validateSubmission(submission, issues, idx) {
-      console.log(">>> [TEST] validateSubmission || submission: ", submission);
       var submission_fields = Object.keys(submission);
-      console.log(">>> [TEST] validateSubmission || submission_fields: ", submission_fields);
-      console.log(">>> [TEST] validateSubmission || expected_submission_fields: ", expected_submission_fields);
       if (submission_fields.length != expected_submission_fields.length) {
-        console.log(">>> [TEST] validateSubmission || return: false");
         return false;
       }
       var errors = [];
@@ -161,7 +157,6 @@ function Header(props) {
       let each_submission = input[i];
       await validateSubmission(each_submission, issues, i);
     }
-    console.log(">>> [TEST] validateSubmission || issues:", issues);
     return issues;
   }
   // Necessary States for Alert Message
@@ -220,14 +215,12 @@ function Header(props) {
       // After validating the JSON, if we find any issues (client side)
       // such as missing fields or invalid entries, show Alert message
       if (Object.keys(ret_issues).length > 0) {
-        console.log(">>> [TEST] createNewSubmission || ret_issues > 0: ", ret_issues);
         // Issues found with JSON
         setValidateStatus("error");
         setValidateStatusMessage("Issues found with JSON data!");
         setValidated(true); // Show the first Alert in the modal
         return;
       }
-      console.log(">>> [TEST] createNewSubmission || outside if ");
       setValidateStatus("success");
       setValidateStatusMessage("No issues found with JSON data!");
       setValidated(true);
@@ -254,7 +247,6 @@ function Header(props) {
 
       let resJson = await res.json();
       if (res.status === 200) {
-        console.log(">>> [TEST] createNewSubmission || success");
         setSeverity("success");
         setMessage("Added batch of submissions successfully!");
         handleCloseSubmission();
@@ -262,7 +254,6 @@ function Header(props) {
         setShowProgress(false);
         return;
       } else {
-        console.log(">>> [TEST] createNewSubmission || failed : ", resJson.message);
         setSubmitBatchStatus("error");
         setSubmitBatchMessage("Issues found with some submissions.");
         setFoundIssues(JSON.stringify(resJson.message));
